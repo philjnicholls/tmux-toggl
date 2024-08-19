d76816c658baddf42b6a969a649744f4cf7ba315
Phil Nicholls <phil.j.nicholls@gmail.com>
2024-08-19 14:56:20 +0800

    FIX: prevent shell locking for running time entry script

--- scripts/running_time_entry.sh
+++ scripts/running_time_entry.sh
@@ -3,5 +3,4 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
-MESSAGE=$(eval "$TOGGL now | head -1")
-tmux display-message "$MESSAGE"
+tmux display-message "$(eval "$TOGGL now | head -1")"
